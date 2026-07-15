c5e81d5772ec7ea36e1c249cda1b536eab205f47
Pratik Pala <pratikpala9999@gmail.com>
2026-07-16 03:58:01 +0530

    fix: transpilation of sentry in mobile tests

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: ['./src/__tests__/setup.ts'],
   transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|lucide-react-native)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|@sentry|native-base|react-native-svg|lucide-react-native)',
   ],
   collectCoverage: false,
   moduleNameMapper: {
